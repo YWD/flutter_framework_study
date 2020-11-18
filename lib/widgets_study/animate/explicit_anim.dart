1e36a38bf7eb49c1d167643e7a0e973d1b30fbe9
杨文冬 <1347011912@qq.com>
2020-11-18 11:47:19 +0800

    tween use

--- lib/widgets_study/animate/explicit_anim.dart
+++ lib/widgets_study/animate/explicit_anim.dart
@@ -10,11 +10,13 @@ class ExplicitAnim extends StatefulWidget {
 
 class _ExplicitAnimState extends State<ExplicitAnim> with SingleTickerProviderStateMixin {
   AnimationController controller;
+  Animation<AlignmentGeometry> alignAnimation;
 
   @override
   void initState() {
     super.initState();
     controller = AnimationController(vsync: this, duration: Duration(seconds: 3));
+    alignAnimation = AlignmentGeometryTween(begin: Alignment.centerLeft, end: Alignment.centerRight).animate(controller);
   }
 
   @override
@@ -34,6 +36,24 @@ class _ExplicitAnimState extends State<ExplicitAnim> with SingleTickerProviderSt
             turns: controller,
             child: FlutterLogo(
               size: 200,
+            ),
+          ),
+          AlignTransition(alignment: alignAnimation, child: Container(width: 200, height: 200, color: Colors.cyan,)),
+          AnimatedBuilder(
+            animation: controller,
+            builder: (context, child) {
+              return Container(
+                width: 200 * controller.value,
+                height: 200 * controller.value,
+                color: Colors.purple,
+                // child: child,
+                child: FlutterLogo(
+                  size: 75,
+                ),
+              );
+            },
+            child: FlutterLogo(
+              size: 75,
             ),
           ),
         ],
